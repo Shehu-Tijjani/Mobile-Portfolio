3f732cad55fa00032962bde48f69cc72d1b13d48
Apple <apple@Apples-MacBook-Pro.local>
2025-09-23 20:28:09 +0100

    new developements

--- script.js
+++ script.js
@@ -4,37 +4,32 @@ const nav = document.querySelector("nav");
 const overlay = document.querySelector(".overlay");
 const imagesSlides = document.querySelector(".images-slides");
 
-const terminateNavList = function() {
-    nav.querySelector(".nav-lists").classList.remove("show-nav-lists");
-    
-    nav.querySelector(".hamburger").classList.remove("hide-hamburger");
-    
-    nav.querySelector(".cancel").classList.remove("show-cancel");
-    
-    nav.querySelector(".overlay").classList.remove("show-overlay");
-}
+const terminateNavList = function () {
+  nav.querySelector(".nav-lists").classList.remove("show-nav-lists");
 
-nav.addEventListener("click", e => {
-    if (e.target.closest(".hamburger-menu")) {
-        nav.querySelector(".nav-lists").classList.toggle("show-nav-lists");
-        
-        nav.querySelector(".hamburger").classList.toggle("hide-hamburger");
-        
-        nav.querySelector(".cancel").classList.toggle("show-cancel");
-        
-        nav.querySelector(".overlay").classList.toggle("show-overlay");
-    }
-    
-    if (e.target.classList.contains("overlay")) {
-        terminateNavList()
-    }
-    
-    if (e.target.classList.contains("nav-link")) {
-        terminateNavList()
-    }
+  nav.querySelector(".hamburger").classList.remove("hide-hamburger");
+
+  nav.querySelector(".cancel").classList.remove("show-cancel");
+
+  nav.querySelector(".overlay").classList.remove("show-overlay");
+};
+
+nav.addEventListener("click", (e) => {
+  if (e.target.closest(".hamburger-menu")) {
+    nav.querySelector(".nav-lists").classList.toggle("show-nav-lists");
+
+    nav.querySelector(".hamburger").classList.toggle("hide-hamburger");
+
+    nav.querySelector(".cancel").classList.toggle("show-cancel");
+
+    nav.querySelector(".overlay").classList.toggle("show-overlay");
+  }
+
+  if (e.target.classList.contains("overlay")) {
+    terminateNavList();
+  }
+
+  if (e.target.classList.contains("nav-link")) {
+    terminateNavList();
+  }
 });
-
-// Doenload resume
-
-a.addEventListener('click', function() {
-})
